ec8d9d0c62df1a374da2eca9b5d353e0b481711d
francaparroz21 <francaparroz21@gmail.com>
2023-04-06 02:22:00 -0300

    listos los metodos de la ruta de 'products', faltan los de la ruta 'carts'

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -6,31 +6,91 @@ import fs from "fs"
 const router = Router();
 
 //Get all products.
-router.get("/",async(req,res)=>{
-    const products = JSON.parse(await fs.promises.readFile('./src/files/products.json','utf-8'))
+router.get("/", async (req, res) => {
+    const products = JSON.parse(await fs.promises.readFile('./src/files/products.json', 'utf-8'))
     const limit = req.query.limit
 
-    if (!limit) return res.send({
-        status: 'Ok',
-        products
-    })
+    if (!limit) return res.status(200).send({ products })
+    else if (limit >= 0 && limit <= products.length) {
+        const productsLimited = products.slice(0, parseInt(limit))
+        return res.send({ productsLimited })
+    }
 
-    const productsLimited = products.slice(0,parseInt(limit))
-
-    res.send({
-        status: 'Ok',
-        productsLimited
+    res.status(400).send({
+        error: `Bad request, limit '${limit}' incorrect.`
     })
 })
 
 //Get product by ID.
 router.get('/:pid', async (req, res) => {
     let pid = req.params.pid
-    let products = await fs.promises.readFile('./src/files/products.json','utf-8')
+    let products = JSON.parse(await fs.promises.readFile('./src/files/products.json', 'utf-8'))
     let product = products.find(product => product.id === parseInt(pid))
     if (!product) return res.status.send(404)
     res.send({ product })
 })
+
+//Post product.
+router.post('/', async (req, res) => {
+    let products = JSON.parse(await fs.promises.readFile('./src/files/products.json', 'utf-8'))
+    let product = req.body
+    let idProduct = products.length + 1
+    let newProducts = [...products, { id: idProduct, thumbnails: [...product.thumbnails], status: true, ...product }]
+
+    if (!product.title || !product.description || !product.code || !product.price || !product.stock || !product.category) {
+        return res.status(400).send({
+            error: 'Required fields: \n -title\n -description \n -code \n -price \n -stock \n -category'
+        })
+    }
+
+    await fs.promises.writeFile('./src/files/products.json', JSON.stringify(newProducts, null, '\t'))
+
+    res.send({
+        status: 'Ok',
+        newProducts
+    })
+})
+
+
+//Put any product.
+router.put('/:pid', async (req, res) => {
+    let idParams = req.params.pid
+    let updates = req.body
+    let products = JSON.parse(await fs.promises.readFile('./src/files/products.json', 'utf-8'))
+    let product = products.find(product => product.id === parseInt(idParams))
+
+    if (!updates.title && !updates.description && !updates.code && !updates.price && !updates.stock && !updates.category) {
+        return res.status(400).send({
+            error: 'Required some of these fields: \n -title\n -description \n -code \n -price \n -stock \n -category'
+        })
+    }
+
+    let updateProduct = { ...product, ...updates }
+    products.splice(products.indexOf(product), 1, updateProduct)
+
+    res.send({
+        updateProduct
+    })
+})
+
+//Delete product by ID.
+router.delete('/:pid', async (req, res) => {
+    let idParams = req.params.pid
+    let products = JSON.parse(await fs.promises.readFile('./src/files/products.json', 'utf-8'))
+    let product = products.find(product => product.id === parseInt(idParams))
+
+    if(!product)res.status(421).send({
+        error: `Misdirect ${idParams} params.`
+    })
+
+    products.splice(product,1)
+    await fs.promises.writeFile('./src/files/products.json', JSON.stringify(products, null, '\t'))
+
+    res.send({
+        products
+    })
+})
+
 
 export default router;
 
